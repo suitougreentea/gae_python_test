18bb9b22f3be8f6dae362785b1c8cc7bc9ebc0b0
suitougreentea <suitougreentea@users.noreply.github.com>
2013-10-26 00:12:37 +0900

    Added contents using bootstrap and GQL.

--- main.py
+++ main.py
@@ -16,9 +16,16 @@
 #
 from google.appengine.api import users
 import os
+import cgi
 from google.appengine.ext.webapp import template
+from google.appengine.ext import db
 
 import webapp2
+
+class TestDB(db.Model):
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
 # User mypage (redirect to User Class)
 class MainHandler(webapp2.RequestHandler):
@@ -36,17 +43,38 @@ class MainHandler(webapp2.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(path, template_values))
 
-# Register user
+# Register
 class RegisterHandler(webapp2.RequestHandler):
-  # if not user[google_user], redirect to register
   def get(self):
     self.response.set_status(403)
     # self.response.write('INPUT')
   def post(self):
+    tdb = TestDB()
+
+    if users.get_current_user():
+        tdb.author = users.get_current_user()
+
+    tdb.content = self.request.get('content')
+    tdb.put()
+
     self.response.write(self.request.get('content'))
+
+# View database
+class ViewHandler(webapp2.RequestHandler):
+  def get(self):
+    tdbs = db.GqlQuery("SELECT * FROM TestDB ORDER BY date DESC LIMIT 10")
+
+    for tdb in tdbs:
+      if tdb.author:
+        self.response.out.write('%s : ' % tdb.author.nickname())
+      else:
+        self.response.out.write('Anonymous : ')
+      self.response.out.write('%s' % cgi.escape(tdb.content))
+
 
 # Like Twitter url mapping is best
 app = webapp2.WSGIApplication([
   ('/', MainHandler),
   ('/register', RegisterHandler),
+  ('/view', ViewHandler),
 ], debug=True)
